a7ab45981544280952c12cecc68c0ddea3df9490
von_Krolikoff <milkbrother@yandex.ru>
2022-06-04 16:45:44 +0300

    Добавлен рандом в EX003

--- EX003_Sum/Program.cs
+++ EX003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA=5;
-int numberB=10;
+﻿int numberA=new Random().Next(1,10); //1 2 3 4 .... 9
+int numberB=new Random().Next(1,10);
+Console.WriteLine (numberA);
+Console.WriteLine (numberB);
 int result=numberA+numberB;
 Console.WriteLine (result);
